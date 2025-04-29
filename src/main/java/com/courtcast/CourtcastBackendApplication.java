98ef6ddecf4f17c352a04ac07249ae9d2c24d497
Hananiah Davis <hananiahd97@gmail.com>
2025-04-29 00:19:10 -0400

    added main class for the spring boot maven plugin

---
+++ src/main/java/com/courtcast/CourtcastBackendApplication.java
@@ -0,0 +1,14 @@
+package com.courtcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * File created by Hananiah Davis on Apr 29, 2025
+ */
+@SpringBootApplication
+public class CourtcastBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourtcastBackendApplication.class, args);
+    }
+}
